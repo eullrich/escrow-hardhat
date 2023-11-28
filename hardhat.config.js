cab8751aeaabf547b4731f13f972cd4d45513ddb
eullrich <eullrich@gmail.com>
2023-11-28 17:38:56 -0500

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 require('@nomicfoundation/hardhat-toolbox');
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
   paths: {
     artifacts: "./app/src/artifacts",
   }
